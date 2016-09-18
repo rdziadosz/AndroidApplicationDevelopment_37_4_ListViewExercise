98b1cb46153076db34231df1e575d6af64b6686e
Radosław <radoslaw@dziadosz.eu>
2016-09-18 18:59:04 +0300

    Simple ListView

--- app/src/main/java/eu/dziadosz/listviewexercise/MainActivity.java
+++ app/src/main/java/eu/dziadosz/listviewexercise/MainActivity.java
@@ -2,6 +2,10 @@ package eu.dziadosz.listviewexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find list view
+        ListView listview = (ListView) findViewById(R.id.listView);
+
+        // generate some dummy data
+        String[] phones = new String[]{
+                "Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu",
+                "Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu"
+        };
+
+        // add data to ArrayList
+        final ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < phones.length; ++i) {
+            list.add(phones[i]);
+        }
+
+        // add data to ArrayAdapter (default Android ListView style/layout)
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+
+        // set data to listView with adapter
+        listview.setAdapter(adapter);
+
     }
 }
